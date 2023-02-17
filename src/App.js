84e85a28be91d3aed6510d8e1b89b0979bcc38e4
hasanalierk <hasanalierk98@gmail.com>
2023-02-17 15:09:02 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+
+import data from "./helper/data";
+import "./styles/style.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1> İMAGE GALLERY </h1>
+      <div className="container">
+      {data.map((item, i) => {
+        const {
+          photographer,
+          src: { large },
+        } = item;
+        // console.log(photographer)
+        // console.log(large)
+        return (
+          <div className="map-div" key={i}>
+            <img className="picture" src={large} alt="" />
+            <div className="photographer">{photographer}</div>
+          </div>
+        );
+      })}
+      </div>
+      
+      
+      
     </div>
   );
 }
 
 export default App;
+//! en son desc yaparken kaldın ordan devam edersin
